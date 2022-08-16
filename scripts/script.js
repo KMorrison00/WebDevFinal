a8b932d780b00b885aea776e9e7f084d02828922
KMorrison00 <kylem.workemail@gmail.com>
2022-08-15 23:39:29 -0700

    consistent header bar with animation

--- scripts/script.js
+++ scripts/script.js
@@ -5,6 +5,7 @@ const articleCards = Array.from(document.querySelectorAll(".article"));
 // this will be nessecary for being able to select text without click an article
 const isTextSelected = window.getSelection().toString();
 // function for clicking an entire article while allowing text selection
+
 //[1] C. Coyier, Block Links: The Search for a Perfect Solution May 25, 2020
 // https://css-tricks.com/block-links-the-search-for-a-perfect-solution/#method-4-sprinkle-javascript-on-the-second-method
 function handleArticleClick(event) {
